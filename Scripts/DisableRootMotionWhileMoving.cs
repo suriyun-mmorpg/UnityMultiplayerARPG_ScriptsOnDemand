6a094a32fbf6ca585ef2062918998f12ee43646a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-11-16 00:52:26 +0700

    Add simple component which will make animation don't update root motion position while character moving

---
+++ Scripts/DisableRootMotionWhileMoving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MultiplayerARPG;
+
+public class DisableRootMotionWhileMoving : MonoBehaviour
+{
+    Animator animator;
+    BaseCharacterEntity character;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        character = GetComponent<BaseCharacterEntity>();
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (character.MovementState.HasFlag(MovementState.IsGrounded) &&
+            !character.MovementState.HasFlag(MovementState.Forward) &&
+            !character.MovementState.HasFlag(MovementState.Backward) &&
+            !character.MovementState.HasFlag(MovementState.Left) &&
+            !character.MovementState.HasFlag(MovementState.Right))
+            animator.ApplyBuiltinRootMotion();
+    }
+}
