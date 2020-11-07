47f833464c87fd41592967857748d2a627dc8362
Insthync <ittipon.bay@gmail.com>
2020-11-08 05:29:57 +0700

    Add pvp kill reward sample extension

---
+++ Scripts/PvpGetGoldExtension/BasePlayerCharacterEntity_PvpGetGold.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class BasePlayerCharacterEntity
+    {
+        [DevExtMethods("Awake")]
+        public void Awake_PvpGetGold()
+        {
+            onReceivedDamage += ReceivedDamage_PvpGetGold;
+        }
+
+        [DevExtMethods("OnDestroy")]
+        public void OnDestroy_PvpGetGold()
+        {
+            onReceivedDamage -= ReceivedDamage_PvpGetGold;
+        }
+
+        private void ReceivedDamage_PvpGetGold(
+            Vector3 fromPosition,
+            IGameEntity attacker,
+            CombatAmountType combatAmountType,
+            int damage,
+            CharacterItem weapon,
+            BaseSkill skill,
+            short skillLevel)
+        {
+            if (!IsServer)
+                return;
+
+            if (attacker == null || !(attacker.Entity is BasePlayerCharacterEntity))
+                return;
+
+            int rewardGold = 100;
+            BasePlayerCharacterEntity character = (attacker.Entity as BasePlayerCharacterEntity);
+            if (!this.IsDead())
+                return;
+
+            character.Gold += rewardGold;
+            CurrentGameManager.SendNotifyRewardGold(character.ConnectionId, rewardGold);
+        }
+    }
+}
